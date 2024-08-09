94cdf5e271f5c8f33e56eab33991780fc21d8882
WinnFreeza27 <erwinpratamaeyr7@gmail.com>
2024-08-09 22:14:17 +0800

    Fix Origins

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
     'http://localhost:3000', 'http://127.0.0.1:3000',
     'https://skm-dinkes.vercel.app', 'localhost:3000', 'http://localhost:3000/',
-    'http://localhost:3000/responses']
+    'http://localhost:3000/responses', 'https://skm-dinkes.vercel.app/responses', 'https://skm-dinkes.vercel.app/']
