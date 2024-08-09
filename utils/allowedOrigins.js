ead5d2e166a81d8b47ffb79ea0643dfb5a317bc7
WinnFreeza27 <erwinpratamaeyr7@gmail.com>
2024-08-09 22:45:39 +0800

    Removing developement features

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,4 +1,10 @@
-export const allowedOrigins = [
-    'http://localhost:3000', 'http://127.0.0.1:3000',
-    'https://skm-dinkes.vercel.app', 'localhost:3000', 'http://localhost:3000/',
-    'http://localhost:3000/responses', 'https://skm-dinkes.vercel.app/responses', 'https://skm-dinkes.vercel.app/']
+// developement
+
+// export const allowedOrigins = [
+//     'http://localhost:3000', 'http://127.0.0.1:3000',
+//     'https://skm-dinkes.vercel.app', 'localhost:3000', 'http://localhost:3000/',
+//     'http://localhost:3000/responses', 'https://skm-dinkes.vercel.app/responses', 'https://skm-dinkes.vercel.app/']
+
+    // production
+
+    export const allowedOrigins = ['https://skm-dinkes.vercel.app/responses', 'https://skm-dinkes.vercel.app/', 'https://skm-dinkes.vercel.app']
